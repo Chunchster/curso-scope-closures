728c5bd1cc06ce5e10c39484f844652a4f1e7bbb
Chunchster <ahuancasc@gmail.com>
2022-07-16 12:24:25 -0500

    Primera subida

---
+++ src/scope/global.js
@@ -0,0 +1,24 @@
+//variables
+
+var a;          //declarando
+var b = 'b';    //declarando / asignamos
+b = 'bb';       //reasignacion
+var a = 'aa';   //redeclaracion
+
+//Global scope
+var fruit = 'Apple'; //global
+console.log(fruit);
+
+function bestFruit(){
+    console.log(fruit);
+}
+
+bestFruit();
+
+function countries(){
+    country = 'Colombia';
+    console.log(country);
+}
+
+countries();
+console.log(country);
