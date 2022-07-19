3fd3fe5b07b3a127d227661434f2c783972e6d6e
Chunchster <ahuancasc@gmail.com>
2022-07-19 05:05:56 -0500

    Segunda actualizacion

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+var firstName; //Undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = 'David'; //Declaramos / asignar
+lastName = 'Ana'; //reasignamos
+console.log(lastName);
+
+var secondName = 'David';   //Declarando / asignando
+var secondName = 'Ana';     //Reasignando
+console.log(secondName);
+
+//let
+
+let fruit = 'Apple';    //declara y asignar
+fruit = 'Kiwi';         //reasignamos
+console.log(fruit);
+
+//let fruit = 'Banana'; //No puede redeclararse
+console.log(fruit);
+
+//const
+const animal = 'Dog';   //declara y asigba
+//animal = 'Cat';         //no puede reasignarse
+//const animal = 'Snake'; //no puede redeclarse
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("😎");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
